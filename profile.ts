557e2db5ce596396a8f7f67144873b0c2fa0a2fd
tasadar2 <tassadar@wowway.com>
2019-03-30 17:34:00 -0400

    Update and rename profile.js to profile.ts

---
+++ profile.ts
@@ -0,0 +1,27 @@
+export class Profile {
+
+    /**
+     * Exports the current profile to a file
+     */
+    public static exportProfile() {
+        File.downloadFile("BondageCollegeProfile.json", JSON.stringify(localStorage));
+    }
+
+    /**
+     * Imports a profile from a file
+     */
+    public static importProfile() {
+        File.openFile((content) => Profile.importProfileContent(content));
+    }
+
+    private static importProfileContent(content: string) {
+        const data = JSON.parse(content);
+
+        for (const dataKey in data) {
+            if (data.hasOwnProperty(dataKey)) {
+                localStorage[dataKey] = data[dataKey];
+            }
+        }
+    }
+
+}
